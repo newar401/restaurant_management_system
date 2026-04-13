d63edd19a86bf2afa674846ec6d88dde18fafb2c
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2026-04-13 20:23:03 +0545

    Part 1: Base system

--- main.cpp
+++ main.cpp
@@ -1,50 +1,32 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
 
-const int FOOD = 15;
-const int COL = 4;
+bool login(){
+    string pass;
+    cout << "Enter manager password: ";
+    cin >> pass;
+
+    if(pass == "admin"){
+        cout << "Access granted\n";
+        return true;
+    }else{
+        cout << "Wrong password\n";
+        return false;
+    }
+}
 
 int main(){
-    string menu[FOOD][COL];
-    int count = 0;
-
-    ifstream in("MenuFood.txt");
-
-    while(getline(in, menu[count][0], '\t') &&
-          getline(in, menu[count][1], '\t') &&
-          getline(in, menu[count][2], ' ') &&
-          getline(in, menu[count][3]))
-    {
-        count++;
-    }
-    in.close();
-
-    for(int i = 0; i < count; i++){
-        cout << i+1 << ". " << menu[i][0]
-             << " RM " << menu[i][1] << endl;
-    }
-
     int choice;
-    double newPrice;
 
-    cout << "Select item: ";
+    cout << "===== RESTAURANT =====\n";
+    cout << "1. Manager\n2. Customer\n3. Exit\n";
     cin >> choice;
 
-    cout << "New price: ";
-    cin >> newPrice;
-
-    ofstream out("MenuFood.txt");
-
-    for(int i = 0; i < count; i++){
-        if(i == choice-1){
-            out << menu[i][0] << "\t" << newPrice << "\t"
-                << menu[i][2] << " " << menu[i][3] << endl;
-        } else {
-            out << menu[i][0] << "\t" << menu[i][1] << "\t"
-                << menu[i][2] << " " << menu[i][3] << endl;
-        }
+    if(choice == 1){
+        login();
+    }
+    else if(choice == 2){
+        cout << "Customer Mode\n";
     }
 
     return 0;
