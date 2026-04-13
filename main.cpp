cc0737815a8e47b3aea1a50f50a798f511764bd9
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2026-04-13 20:31:19 +0545

    Part 2: Menu loading

--- main.cpp
+++ main.cpp
@@ -1,32 +1,53 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+const int MAX = 15;
 
 bool login(){
     string pass;
     cout << "Enter manager password: ";
     cin >> pass;
 
-    if(pass == "admin"){
-        cout << "Access granted\n";
-        return true;
-    }else{
-        cout << "Wrong password\n";
-        return false;
+    return pass == "admin";
+}
+
+void loadMenu(string name[], double price[], int time[], int stock[], int &n){
+    ifstream in("MenuFood.txt");
+    n = 0;
+
+    while(in >> name[n] >> price[n] >> time[n] >> stock[n]){
+        n++;
+    }
+}
+
+void showMenu(string name[], double price[], int time[], int stock[], int n){
+    cout << "\n===== MENU =====\n";
+    for(int i=0;i<n;i++){
+        cout << i+1 << ". " << name[i]
+             << " RM " << price[i]
+             << " Time:" << time[i]
+             << " Stock:" << stock[i] << endl;
     }
 }
 
 int main(){
     int choice;
 
+    string name[MAX];
+    double price[MAX];
+    int time[MAX], stock[MAX];
+    int n;
+
     cout << "===== RESTAURANT =====\n";
-    cout << "1. Manager\n2. Customer\n3. Exit\n";
     cin >> choice;
 
     if(choice == 1){
         login();
     }
     else if(choice == 2){
-        cout << "Customer Mode\n";
+        loadMenu(name, price, time, stock, n);
+        showMenu(name, price, time, stock, n);
     }
 
     return 0;
