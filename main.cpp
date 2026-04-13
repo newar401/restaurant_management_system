08e3eb5bf4b37fcd91f1a3f43eccc8d93277dd54
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2026-04-13 20:33:13 +0545

    Part 3: Add food

--- main.cpp
+++ main.cpp
@@ -6,9 +6,7 @@ const int MAX = 15;
 
 bool login(){
     string pass;
-    cout << "Enter manager password: ";
     cin >> pass;
-
     return pass == "admin";
 }
 
@@ -22,30 +20,39 @@ void loadMenu(string name[], double price[], int time[], int stock[], int &n){
 }
 
 void showMenu(string name[], double price[], int time[], int stock[], int n){
-    cout << "\n===== MENU =====\n";
     for(int i=0;i<n;i++){
-        cout << i+1 << ". " << name[i]
-             << " RM " << price[i]
-             << " Time:" << time[i]
-             << " Stock:" << stock[i] << endl;
+        cout << i+1 << ". " << name[i] << endl;
     }
+}
+
+void addFood(){
+    ofstream out("MenuFood.txt", ios::app);
+
+    string name;
+    double price;
+    int time, stock;
+
+    cout << "Name: "; cin >> name;
+    cout << "Price: "; cin >> price;
+    cout << "Time: "; cin >> time;
+    cout << "Stock: "; cin >> stock;
+
+    out << name << " " << price << " " << time << " " << stock << endl;
 }
 
 int main(){
     int choice;
-
-    string name[MAX];
-    double price[MAX];
-    int time[MAX], stock[MAX];
-    int n;
-
-    cout << "===== RESTAURANT =====\n";
     cin >> choice;
 
     if(choice == 1){
-        login();
+        if(login()) addFood();
     }
     else if(choice == 2){
+        string name[MAX];
+        double price[MAX];
+        int time[MAX], stock[MAX];
+        int n;
+
         loadMenu(name, price, time, stock, n);
         showMenu(name, price, time, stock, n);
     }
