812e7389955b34b60ca8d37b613672bc8647520b
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2026-04-22 10:22:22 +0545

    added restaurant system OOP version

--- main.cpp
+++ main.cpp
@@ -5,211 +5,211 @@ using namespace std;
 
 const int MAX = 15;
 
-// LOGIN
-bool login(){
-    string pass;
-    cout << "Enter manager password: ";
-    cin >> pass;
-
-    if(pass == "admin"){
-        cout << "Access granted\n";
-        return true;
-    }else{
-        cout << "Wrong password\n";
-        return false;
-    }
-}
-
-// LOAD MENU
-void loadMenu(string name[], double price[], int time[], int stock[], int &n){
-    ifstream in("MenuFood.txt");
-    n = 0;
-
-    if(!in){
-        cout << "Menu file not found!\n";
-        return;
-    }
-
-    while(n < MAX && in >> name[n] >> price[n] >> time[n] >> stock[n]){
-        n++;
-    }
-    in.close();
-}
-
-// SAVE MENU
-void saveMenu(string name[], double price[], int time[], int stock[], int n){
-    ofstream out("MenuFood.txt");
-
-    for(int i=0;i<n;i++){
-        out << name[i] << " "
-            << price[i] << " "
-            << time[i] << " "
-            << stock[i] << endl;
-    }
-
-    out.close();
-}
-
-// SHOW MENU
-void showMenu(string name[], double price[], int time[], int stock[], int n){
-    cout << "\n==============================\n";
-    cout << "           MENU\n";
-    cout << "==============================\n";
-
-    for(int i=0;i<n;i++){
-        cout << i+1 << ". " << name[i]
-             << " | RM " << price[i]
-             << " | Time: " << time[i]
-             << " | Stock: " << stock[i] << endl;
-    }
-
-    cout << "==============================\n";
-}
-
-// ADD FOOD
-void addFood(){
-    ofstream out("MenuFood.txt", ios::app);
-
-    string name;
-    double price;
-    int time, stock;
-    char ch;
-
-    do{
-        cout << "Name: ";
-        cin >> ws;
-        getline(cin, name);
-
-        cout << "Price: ";
-        cin >> price;
-
-        cout << "Time: ";
-        cin >> time;
-
-        cout << "Stock: ";
-        cin >> stock;
-
-        out << name << " " << price << " " << time << " " << stock << endl;
-
-        cout << "Add more? (Y/N): ";
-        cin >> ch;
-
-    }while(ch=='Y'||ch=='y');
-
-    out.close();
-}
-
-// UPDATE PRICE
-void updatePrice(){
+class Restaurant {
+private:
     string name[MAX];
     double price[MAX];
-    int time[MAX], stock[MAX];
+    int timeArr[MAX];
+    int stock[MAX];
     int n;
 
-    loadMenu(name, price, time, stock, n);
-    showMenu(name, price, time, stock, n);
-
-    int choice;
-    cout << "Select food number: ";
-    cin >> choice;
-
-    if(choice < 1 || choice > n){
-        cout << "Invalid choice\n";
-        return;
+public:
+    Restaurant(){
+        n = 0;
+        loadMenu();
     }
 
-    cout << "New price: ";
-    cin >> price[choice-1];
+    ~Restaurant(){
+        saveMenu();
+    }
 
-    saveMenu(name, price, time, stock, n);
+    bool login(){
+        string pass;
+        cout << "Enter manager password: ";
+        cin >> pass;
 
-    cout << "Price updated!\n";
-}
+        if(pass == "admin"){
+            cout << "Access granted\n";
+            return true;
+        }else{
+            cout << "Wrong password\n";
+            return false;
+        }
+    }
 
-// CUSTOMER SYSTEM
-void customer(){
-    string name[MAX];
-    double price[MAX];
-    int time[MAX], stock[MAX];
-    int n;
+    void loadMenu(){
+        ifstream in("MenuFood.txt");
+        n = 0;
 
-    loadMenu(name, price, time, stock, n);
+        if(!in){
+            cout << "Menu file not found!\n";
+            return;
+        }
 
-    double total = 0;
-    int totalTime = 0;
-    int popular[MAX] = {0};
+        while(n < MAX && in >> name[n] >> price[n] >> timeArr[n] >> stock[n]){
+            n++;
+        }
 
-    int choice;
-    char more;
+        in.close();
+    }
 
-    do{
-        showMenu(name, price, time, stock, n);
+    void saveMenu(){
+        ofstream out("MenuFood.txt");
 
-        cout << "Select food: ";
+        for(int i=0;i<n;i++){
+            out << name[i] << " "
+                << price[i] << " "
+                << timeArr[i] << " "
+                << stock[i] << endl;
+        }
+
+        out.close();
+    }
+
+    void showMenu(){
+        cout << "\n==============================\n";
+        cout << "           MENU\n";
+        cout << "==============================\n";
+
+        for(int i=0;i<n;i++){
+            cout << i+1 << ". " << name[i]
+                 << " | RM " << price[i]
+                 << " | Time: " << timeArr[i]
+                 << " | Stock: " << stock[i] << endl;
+        }
+
+        cout << "==============================\n";
+    }
+
+    void addFood(){
+        ofstream out("MenuFood.txt", ios::app);
+
+        string nm;
+        double pr;
+        int t, st;
+        char ch;
+
+        do{
+            cout << "Name: ";
+            cin >> ws;
+            getline(cin, nm);
+
+            cout << "Price: ";
+            cin >> pr;
+
+            cout << "Time: ";
+            cin >> t;
+
+            cout << "Stock: ";
+            cin >> st;
+
+            out << nm << " " << pr << " " << t << " " << st << endl;
+
+            cout << "Add more? (Y/N): ";
+            cin >> ch;
+
+        }while(ch=='Y'||ch=='y');
+
+        out.close();
+        loadMenu();
+    }
+
+    void updatePrice(){
+        showMenu();
+
+        int choice;
+        cout << "Select food number: ";
         cin >> choice;
 
-        int i = choice - 1;
-
-        if(i < 0 || i >= n){
+        if(choice < 1 || choice > n){
             cout << "Invalid choice\n";
-            continue;
+            return;
         }
 
-        if(stock[i] <= 0){
-            cout << "Out of stock!\n";
-            continue;
-        }
+        cout << "New price: ";
+        cin >> price[choice-1];
 
-        stock[i]--;
-        popular[i]++;
-
-        total += price[i];
-        totalTime += time[i];
-
-        cout << "Total: RM " << total << endl;
-        cout << "Delivery Time: " << totalTime << " min\n";
-
-        cout << "Order more? (Y/N): ";
-        cin >> more;
-
-    }while(more=='Y'||more=='y');
-
-    if(total == 0){
-        cout << "No orders made.\n";
-        return;
+        saveMenu();
+        cout << "Price updated!\n";
     }
 
-    int maxIndex = 0;
-    for(int j=1;j<n;j++){
-        if(popular[j] > popular[maxIndex]){
-            maxIndex = j;
+    void customer(){
+        double total = 0;
+        int totalTime = 0;
+        int popular[MAX] = {0};
+
+        int choice;
+        char more;
+
+        do{
+            showMenu();
+
+            cout << "Select food: ";
+            cin >> choice;
+
+            int i = choice - 1;
+
+            if(i < 0 || i >= n){
+                cout << "Invalid choice\n";
+                continue;
+            }
+
+            if(stock[i] <= 0){
+                cout << "Out of stock!\n";
+                continue;
+            }
+
+            stock[i]--;
+            popular[i]++;
+
+            total += price[i];
+            totalTime += timeArr[i];
+
+            cout << "Total: RM " << total << endl;
+            cout << "Delivery Time: " << totalTime << " min\n";
+
+            cout << "Order more? (Y/N): ";
+            cin >> more;
+
+        }while(more=='Y'||more=='y');
+
+        if(total == 0){
+            cout << "No orders made.\n";
+            return;
         }
-    }
 
-    double pay;
-    cout << "Enter payment: ";
-    cin >> pay;
+        int maxIndex = 0;
+        for(int j=1;j<n;j++){
+            if(popular[j] > popular[maxIndex]){
+                maxIndex = j;
+            }
+        }
 
-    while(pay < total){
-        cout << "Insufficient amount. Enter again: ";
+        double pay;
+        cout << "Enter payment: ";
         cin >> pay;
+
+        while(pay < total){
+            cout << "Insufficient amount. Enter again: ";
+            cin >> pay;
+        }
+
+        cout << "Payment successful!\n";
+        cout << "Change: RM " << pay - total << endl;
+        cout << "Most popular dish: " << name[maxIndex] << endl;
+
+        saveMenu();
+
+        ofstream out("OutputFile.txt");
+        out << "Total: RM " << total << endl;
+        out << "Delivery Time: " << totalTime << endl;
+        out << "Popular Dish: " << name[maxIndex] << endl;
+        out.close();
     }
+};
 
-    cout << "Payment successful!\n";
-    cout << "Change: RM " << pay - total << endl;
-    cout << "Most popular dish: " << name[maxIndex] << endl;
-
-    saveMenu(name, price, time, stock, n);
-
-    ofstream out("OutputFile.txt");
-    out << "Total: RM " << total << endl;
-    out << "Delivery Time: " << totalTime << endl;
-    out << "Popular Dish: " << name[maxIndex] << endl;
-    out.close();
-}
-
-// MAIN
 int main(){
+    Restaurant r;
     int choice;
 
     do{
@@ -224,22 +224,21 @@ int main(){
         cin >> choice;
 
         if(choice == 1){
-            if(login()){
+            if(r.login()){
                 int m;
                 cout << "1. Add Food\n2. Update Price\n";
                 cin >> m;
 
-                if(m == 1) addFood();
-                else if(m == 2) updatePrice();
+                if(m == 1) r.addFood();
+                else if(m == 2) r.updatePrice();
             }
         }
         else if(choice == 2){
-            customer();
+            r.customer();
         }
 
     }while(choice != 3);
 
     cout << "Thank you!\n";
-
     return 0;
 }
